172dc968f86ab8b01e1d8b727662c5f8bd104b41
Emmanuel Monle <emmanuel.monle@gmail.com>
2023-01-16 10:49:54 +0200

    create model for offender info

---
+++ api/src/models/Offenders.ts
@@ -0,0 +1,27 @@
+import mongoose from 'mongoose'
+
+export type OffenderDocument = {
+    pilotId: string
+    firstName: string
+    lastName: string
+    phoneNumber: string
+    createdDt: string
+    email: string
+    xCoord: number
+    yCoord: number
+    proximity: number
+}
+
+const offenderSchema = new mongoose.Schema({
+    pilotId: String,
+    firstName: String,
+    lastName: String,
+    phoneNumber: String,
+    createdDt: String,
+    email: String,
+    xCoord: Number,
+    yCoord: Number,
+    proximity: Number
+})
+
+export default mongoose.model<OffenderDocument>('Offender', offenderSchema)
